670ad430444ec98e1b90165a9616a865f4cae4db
ghofrane-2005 <ghofranehbila9@gmail.com>
2025-11-18 10:32:38 +0100

    dcm

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Task } from "./task/task";
+import { CurrencyPipe, DatePipe, DecimalPipe, LowerCasePipe, PercentPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { TaskService } from './task-service';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Task],
+  imports: [RouterOutlet, Task,UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe,CurrencyPipe,PercentPipe,DecimalPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+today = new Date();
+
+price = 1234.56;
+percentage = 0.259;
 
 
 
   protected readonly title = signal('todolist');
+tasks: any[] = [];
+constructor(private taskService: TaskService) {}
 
-
-tasks = [
-{ id:1, title: 'Learn Angular', done: false },
-{ id:2,title: 'Build ToDoList App', done: false },
-{ id:3, title: 'Celebrate!', done: true }
-];
-
+ngOnInit() {
+  this.tasks = this.taskService.getTasks();
+}
 addNewTask(){
 
   
